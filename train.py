ce39e8de4f5c4eb1d721592d57f00292d46a1969
Erik van der Plas <hi@erikvanderplas.com>
2018-11-08 23:22:07 +0100

    Made loss function DataParallel

--- train.py
+++ train.py
@@ -49,18 +49,22 @@ if __name__ == '__main__':
 
     model = BSPredictor(hidden_size=128)
 
-    if torch.cuda.device_count() > 1:
-        print("Using %d GPUs for data parallelism" % torch.cuda.device_count())
-        model = nn.DataParallel(model, dim=1)
-
     if os.path.exists(SAVE_PATH):
         print('Loading pre-trained model')
         model.load_state_dict(torch.load(SAVE_PATH))
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model.to(device)
 
     loss_function = LogLoss()
+
+    if torch.cuda.device_count() > 1:
+        print("Using %d GPUs for data parallelism" % torch.cuda.device_count())
+        model = nn.DataParallel(model, dim=1)
+        loss_function = nn.DataParallel(loss_function, dim=1)
+    elif torch.cuda.device_count() == 1:
+        model.to(device)
+        loss_function.to(device)
+
     optimizer = optim.Adam(model.parameters())
 
     test_part = 0.2
